76351203874c7ac662d219fbee3f636e13164a96
Saake <saake.atuma@gmail.com>
2022-09-02 17:04:19 +0100

    Completed Slovi assignment

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_restful import Api
+
+
+from app.controllers.api import (
+    register,
+    login,
+    template,
+    template_1
+)
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(register, '/register')
+api.add_resource(login, '/login')
+api.add_resource(template,'/template')
+api.add_resource(template_1,'/template/<string:temp_id>')
+
+if __name__ == '__main__':
+    app.run(debug = True)
